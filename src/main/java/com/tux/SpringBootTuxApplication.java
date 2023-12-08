fc413b895274b55782939920ee292ef30ccde262
Rodrigo Salazar <rodrigosalad@gmail.com>
2023-12-07 23:53:47 -0500

    init

---
+++ src/main/java/com/tux/SpringBootTuxApplication.java
@@ -0,0 +1,13 @@
+package com.tux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTuxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tux/SpringBootTuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTuxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
