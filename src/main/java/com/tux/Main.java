8e3edbafacd50c4c9bb692b0e01585bac74e60e4
Rodrigo Salazar <rodrigosalad@gmail.com>
2023-12-08 18:36:39 -0500

    verbose

--- src/main/java/com/tux/Main.java
+++ src/main/java/com/tux/Main.java
@@ -1,10 +1,16 @@
 package com.tux;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+// @SpringBootApplication
+@Configuration
+@ComponentScan(basePackages = {"com.tux"})
+@EnableAutoConfiguration
 @RestController
 public class Main {
     public static void main(String[] args) {
