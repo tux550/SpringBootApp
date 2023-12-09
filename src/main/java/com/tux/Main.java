55c22bcbe8bb60e11b0bcee71789dffc4f40dbf7
Rodrigo Salazar <rodrigosalad@gmail.com>
2023-12-08 19:03:55 -0500

    JSON responses

--- src/main/java/com/tux/Main.java
+++ src/main/java/com/tux/Main.java
@@ -7,18 +7,32 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// @SpringBootApplication
-@Configuration
-@ComponentScan(basePackages = {"com.tux"})
-@EnableAutoConfiguration
+import java.util.List;
+
+@SpringBootApplication
+//@Configuration
+//@ComponentScan(basePackages = {"com.tux"})
+//@EnableAutoConfiguration
 @RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
+
+    public record Person(String name, int age) {}
+    public record MyNotesResponse(
+            String title,
+            Person owner,
+            List<String> Notes) {}
+
     @GetMapping("/")
-    public String greet() {
-        return "Hello World!";
+    public MyNotesResponse greet() {
+        return new MyNotesResponse(
+                "Hello",
+                new Person("Tux", 21),
+                List.of("Note 1", "Note 2")
+        );
     }
+
 }
