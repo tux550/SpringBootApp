417256dff2a8d9385ab9dc54d3e73599629b4bf3
Rodrigo Salazar <rodrigosalad@gmail.com>
2023-12-09 20:43:27 -0500

    CRUD operations

--- src/main/java/com/tux/Customer.java
+++ src/main/java/com/tux/Customer.java
@@ -12,7 +12,8 @@ public class Customer {
     @Id
     @SequenceGenerator(
         name = "customer_id_sequence",
-        sequenceName = "customer_id_sequence"
+        sequenceName = "customer_id_sequence",
+        allocationSize = 1
     )
     @GeneratedValue(
         strategy = jakarta.persistence.GenerationType.SEQUENCE,
--- src/main/java/com/tux/Main.java
+++ src/main/java/com/tux/Main.java
@@ -1,38 +1,45 @@
 package com.tux;
+import jakarta.websocket.server.PathParam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @SpringBootApplication
-//@Configuration
-//@ComponentScan(basePackages = {"com.tux"})
-//@EnableAutoConfiguration
 @RestController
+@RequestMapping(path = "api/v1/customers")
 public class Main {
+    private final  CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-
-    public record Person(String name, int age) {}
-    public record MyNotesResponse(
-            String title,
-            Person owner,
-            List<String> Notes) {}
-
-    @GetMapping("/")
-    public MyNotesResponse greet() {
-        return new MyNotesResponse(
-                "Hello",
-                new Person("Tux", 21),
-                List.of("Note 1", "Note 2")
-        );
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
     }
 
+    public record NewCustomerRequest(String name, String email, Integer age) {}
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setName(request.name);
+        customer.setEmail(request.email);
+        customer.setAge(request.age);
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id) {
+        customerRepository.deleteById(id);
+    }
 }
